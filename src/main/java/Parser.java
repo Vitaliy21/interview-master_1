381f2224f2ceb17a4f4e5e27b78a52ac41678f14
Vitaliy Veres <vveres@edunav.com>
2020-04-08 13:07:14 +0300

    implementation result #1

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,13 +1,181 @@
 import net.sf.json.JSONObject;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
+ * The <code>Parser</code> class contains one method 'parse' that returns a generated json with the fields that have differences.
  *
+ * <p>The method expects that the json schema of input objects is constant and contains specified fields: <code>id, meta, candidates<code>.
+ * Whatever additional fields are presented in the input jsons  - report will ignore that and include to the result only predefined fields.
+ *
+ * <p>Also, the method expects that the "meta" field has constant structure: <code>title, startTime, endTime<code>. Otherwise, will throw exception - "meta data has missed fields".
  */
 public class Parser {
 
-    public JSONObject parse(JSONObject before, JSONObject after) {
-        // TODO Implement this
-        return null;
+    private static final String DATE_TIME_FORMAT_PATTERN = "yyyy-MM-dd'T'HH:mm:ssX";
+    private static final String TIME_ZONE = "UTC+2";
+
+    public JSONObject parse(JSONObject before, JSONObject after) throws Exception {
+        Objects.requireNonNull(before, "first input json object must not be null");
+        Objects.requireNonNull(after, "second input json object must not be null");
+
+        JSONObject diffResult = new JSONObject();
+
+        //we don't generate report for elements with different ids
+        if (!before.get(Fields.id.name()).equals(after.get(Fields.id.name()))) {
+            throw new Exception("json objects have different identifiers");
+        }
+
+        //find and add diffs if exist for "meta" block
+        List<Map<String, Object>> metaDiffs = getMetaDataDiff((Map)before.get(Fields.meta.name()), (Map)after.get(Fields.meta.name()));
+        if (!metaDiffs.isEmpty()) {
+            diffResult.put(Fields.meta.name(), metaDiffs);
+        }
+
+        //find and add diffs if exists for "candidates" block
+        Map<String, List<Object>> candidatesDiffs = getCandidatesDataDiff((List)before.get(Fields.candidates.name()), (List)after.get(Fields.candidates.name()));
+        if (!candidatesDiffs.isEmpty()) {
+            diffResult.put(Fields.candidates.name(), candidatesDiffs);
+        }
+        return diffResult;
+    }
+
+    private List<Map<String, Object>> getMetaDataDiff(Map<String, Object> beforeMeta, Map<String, Object> afterMeta) throws Exception {
+        //meta fields must have constant structure with "title", "start time", "end time"
+        verifyMetaFields(beforeMeta);
+        verifyMetaFields(afterMeta);
+
+        List<Map<String, Object>> result = new ArrayList<>();
+        beforeMeta.forEach((key, value) -> {
+            if (!value.equals(afterMeta.get(key))) {
+                Map<String, Object> diff = new LinkedHashMap<>();
+                diff.put(Fields.field.name(), key);
+                //for time fields the timezone should be converted to CEST (Oslo - UTC+2).
+                //not the best way of definition for time fields but for this case is ok imo
+                if (key.toString().contains("Time")) {
+                    diff.put(Fields.before.name(), convertTime(value));
+                    diff.put(Fields.after.name(), convertTime(afterMeta.get(key)));
+                } else {
+                    diff.put(Fields.before.name(), value);
+                    diff.put(Fields.after.name(), afterMeta.get(key));
+                }
+                result.add(diff);
+            }
+        });
+        return result;
+    }
+
+    private void verifyMetaFields(Map<String, Object> meta) throws Exception {
+        Objects.requireNonNull(meta, "meta field is missed");
+        Set<String> fields = meta.keySet();
+        List<String> constFields = Arrays.asList(Fields.title.name(), Fields.startTime.name(), Fields.endTime.name());
+        if(!fields.containsAll(constFields)) {
+            throw new Exception("meta data has missed fields");
+        }
+    }
+
+    private String convertTime(Object dateString) {
+        OffsetDateTime date = OffsetDateTime.parse(dateString.toString());
+        DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                .appendPattern(DATE_TIME_FORMAT_PATTERN)
+                .toFormatter(Locale.ENGLISH);
+        return formatter.withZone(ZoneId.of(TIME_ZONE)).format(date);
+    }
+
+    private Map<String, List<Object>> getCandidatesDataDiff(List<Map<String, Object>> beforeCandidates,
+                                                            List<Map<String, Object>> afterCandidates) {
+        Objects.requireNonNull(beforeCandidates, "candidates field is missed");
+        Objects.requireNonNull(afterCandidates, "candidates field is missed");
+
+        Map<String, List<Object>> result = new LinkedHashMap<>();
+
+        //we need convert "after" collection to Map for faster find elements there (by id).
+        Map<Integer, Object> afterCandidatesMap = afterCandidates.stream()
+                .collect(Collectors.toMap(e -> (Integer) e.get(Fields.id.name()), e -> e));
+
+        //iterate over first collection and try find these elements in converted map by Id
+        beforeCandidates.forEach(map -> {
+            Map<String, Object> elemFromAfterById = (Map)afterCandidatesMap.get(map.get(Fields.id.name()));
+            if (elemFromAfterById == null) {
+                //it means that element not presented in second collection and can be marked as "removed".
+                List<Object> removedElements = result.computeIfAbsent(Fields.removed.name(), value -> new ArrayList<>());
+                removedElements.add(new IdRow(map.get(Fields.id.name())));
+            } else {
+                //otherwise an element presents in both collections and we need compare if they are similar. if not - mark it as "edited".
+                if (hasDifferentFieldValues(map, elemFromAfterById)) {
+                    List<Object> editedElements = result.computeIfAbsent(Fields.edited.name(), value -> new ArrayList<>());
+                    editedElements.add(new IdRow(map.get(Fields.id.name())));
+                }
+            }
+        });
+
+        /*
+        now we need convert "before" collection to Map for faster find elements there (by id).
+        for find and mark items as "added" if they presented in "after" collection and missed in "before"
+        */
+        Map<Integer, Object> beforeCandidatesMap = beforeCandidates.stream()
+                .collect(Collectors.toMap(e -> (Integer) e.get(Fields.id.name()), e -> e));
+        afterCandidates.forEach(map -> {
+            Map<String, Object> elemFromBeforeById = (Map) beforeCandidatesMap.get(map.get(Fields.id.name()));
+            if (elemFromBeforeById == null) {
+                List<Object> addedElements = result.computeIfAbsent(Fields.added.name(), value -> new ArrayList<>());
+                addedElements.add(new IdRow(map.get(Fields.id.name())));
+            }
+        });
+
+        return result;
+    }
+
+    private boolean hasDifferentFieldValues(Map<String, Object> map, Map<String, Object> elemFromAfterById) {
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            if (!value.equals(elemFromAfterById.get(key))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private enum Fields {
+        id,
+        meta,
+        candidates,
+        field,
+        before,
+        after,
+        removed,
+        edited,
+        added,
+        title,
+        startTime,
+        endTime
+    }
+
+    public class IdRow {
+        private Object id;
+
+        protected IdRow(Object id) {
+            this.id = id;
+        }
+
+        public Object getId() {
+            return id;
+        }
+
+
     }
 
 }
--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,17 +1,134 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  *
  */
 public class ParserTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ParserTest.class);
+
     Parser parser = new Parser();
+    private JSONObject before;
+    private JSONObject after;
+    private JSONObject expected;
+    private JSONObject beforeWithWrongId;
+    private JSONObject afterModified;
+    private JSONObject beforeWithoutMetaTitle;
+    private JSONObject afterWithMissedMeta;
+    private JSONObject afterWithMissedCandidates;
+
 
     @Before
     public void setup() {
-        // TODO Load in test data from before.json and after.json
+        try {
+            this.before = readJSONFile("src/test/resources/before.json");
+            this.after = readJSONFile("src/test/resources/after.json");
+            this.expected = readJSONFile("src/test/resources/diff.json");
+
+            this.beforeWithWrongId = readJSONFile("src/test/resources/before_with_wrong_id.json");
+            this.afterModified = readJSONFile("src/test/resources/after_modified.json");
+            this.beforeWithoutMetaTitle = readJSONFile("src/test/resources/before_without_meta_title.json");
+            this.afterWithMissedMeta = readJSONFile("src/test/resources/after_with_missed_meta.json");
+            this.afterWithMissedCandidates = readJSONFile("src/test/resources/after_with_missed_candidates.json");
+
+        } catch (IOException e) {
+            LOGGER.error("Unable to configure tests. Json Error parsing. Caused: ", e);
+        }
     }
 
-    // TODO Define tests here
+    @Test
+    public void testDiffJsonsExpected() throws Exception {
+        JSONObject result = parser.parse(this.before, this.after);
+        Assert.assertTrue(expected.equals(result));
+        LOGGER.info("expected json report: " + expected.toString());
+        LOGGER.info("similar result: " + result.toString());
+        System.out.println();
+    }
+
+    @Test
+    public void testDiffJsonsUnexpected() throws Exception {
+        JSONObject result = parser.parse(this.before, this.afterModified);
+        Assert.assertFalse(expected.equals(result));
+        LOGGER.info("expected json report: " + expected.toString());
+        LOGGER.info("different result: " + result.toString());
+        System.out.println();
+    }
+
+    @Test
+    public void testWithWrongId() {
+        Exception exception = null;
+        try {
+            JSONObject result = parser.parse(this.beforeWithWrongId, this.after);
+        } catch (Exception e) {
+            exception = e;
+        }
+        Assert.assertNotNull(exception);
+        Assert.assertEquals("json objects have different identifiers", exception.getMessage());
+    }
+
+    @Test
+    public void testWithOneNullJson() {
+        NullPointerException exception = null;
+        try {
+            JSONObject result = parser.parse(null, this.after);
+        } catch (Exception e) {
+            exception = (NullPointerException) e;
+        }
+        Assert.assertNotNull(exception);
+        Assert.assertEquals("first input json object must not be null", exception.getMessage());
+    }
+
+    @Test
+    public void testWithMissedMetaTitle() {
+        Exception exception = null;
+        try {
+            JSONObject result = parser.parse(this.beforeWithoutMetaTitle, this.after);
+        } catch (Exception e) {
+            exception = e;
+        }
+        Assert.assertNotNull(exception);
+        Assert.assertEquals("meta data has missed fields", exception.getMessage());
+    }
+
+    @Test
+    public void testWithMissedMeta() {
+        Exception exception = null;
+        try {
+            JSONObject result = parser.parse(this.before, this.afterWithMissedMeta);
+        } catch (Exception e) {
+            exception = e;
+        }
+        Assert.assertNotNull(exception);
+        Assert.assertEquals("meta field is missed", exception.getMessage());
+    }
+
+    @Test
+    public void testWithMissedCandidates() {
+        Exception exception = null;
+        try {
+            JSONObject result = parser.parse(this.before, this.afterWithMissedCandidates);
+        } catch (Exception e) {
+            exception = e;
+        }
+        Assert.assertNotNull(exception);
+        Assert.assertEquals("candidates field is missed", exception.getMessage());
+    }
+
+    private JSONObject readJSONFile(String filename) throws JSONException, IOException {
+        String jsonString = new String(Files.readAllBytes(Paths.get(filename)));
+        JSONObject json = new ObjectMapper().readValue(jsonString, JSONObject.class);
+        return json;
+    }
 
 }
